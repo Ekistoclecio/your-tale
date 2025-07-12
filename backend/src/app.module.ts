cd8f4a1e1b5f30ff1e8cebedb238b8b552f79918
IanKarlo <iankarlots@gmail.com>
2025-07-12 20:28:04 -0300

    chore: Ajuste na organização dos modulos principais

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './api/controllers/app.controller';
+import { AppService } from './core/services/app.service';
+import { UsersModule } from './core/modules/users.module';
+import typeorm from './config/db/database.config';
+import { AuthModule } from './core/modules/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('typeorm')!)
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
