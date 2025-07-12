ab36a99a467fa9dad389c583f6c2d9c8113507f1
IanKarlo <iankarlots@gmail.com>
2025-07-12 20:26:59 -0300

    feat: Adicionando configurações do banco de dados

---
+++ backend/src/config/db/database.config.ts
@@ -0,0 +1,21 @@
+import { registerAs } from "@nestjs/config";
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from "typeorm";
+
+dotenvConfig({ path: '.env' });
+
+const config = {
+    type: 'postgres',
+    host: `${process.env.DB_HOST}`,
+    port: `${process.env.DB_PORT}`,
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    entities: process.env.ENTITIES_PATH ? [process.env.ENTITIES_PATH] : ["*/entities/*.ts"],
+    migrations: process.env.MIGRATIONS_PATH ? [process.env.MIGRATIONS_PATH] : ["*/migrations/*.ts"],
+    autoLoadEntities: true,
+    synchronize: false,
+}
+
+export default registerAs('typeorm', () => config)
+export const connectionSource = new DataSource(config as DataSourceOptions);
