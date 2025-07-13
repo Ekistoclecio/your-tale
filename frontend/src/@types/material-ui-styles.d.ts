b3407e74b683e5a1e4be4c39ae89d01e8119eae1
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-13 17:00:16 -0300

    feat: Adicionar novas opções de tema e tipografia ao Material-UI com bordas e sombras personalizadas

--- frontend/src/@types/material-ui-styles.d.ts
+++ frontend/src/@types/material-ui-styles.d.ts
@@ -16,4 +16,50 @@ declare module '@mui/material/styles' {
   interface PaletteOptions {
     background?: Partial<TypeBackground>;
   }
+  interface Theme {
+    shape: {
+      borderRadius: number;
+      borderRadiusSmall: number;
+      borderRadiusMedium: number;
+      borderRadiusLarge: number;
+      borderRadiusXLarge: number;
+      borderRadiusFull: string;
+    };
+    shadowsCustom: {
+      sm: (color: string) => string;
+      md: (color: string) => string;
+      lg: (color: string) => string;
+      xl: (color: string) => string;
+      glow: (color: string) => string;
+      inset: (color: string) => string;
+    };
+  }
+
+  interface ThemeOptions {
+    spacing: (factor: number) => string;
+    shape: {
+      borderRadius: number;
+      borderRadiusSmall: number;
+      borderRadiusMedium: number;
+      borderRadiusLarge: number;
+      borderRadiusXLarge: number;
+      borderRadiusFull: string;
+    };
+    shadowsCustom: {
+      sm: (color: string) => string;
+      md: (color: string) => string;
+      lg: (color: string) => string;
+      xl: (color: string) => string;
+      glow: (color: string) => string;
+      inset: (color: string) => string;
+    };
+  }
+
+  interface TypographyVariantsOptions {
+    fontWeightSemiBold: number;
+  }
+
+  interface TypographyVariants {
+    fontWeightSemiBold: number;
+  }
 }
