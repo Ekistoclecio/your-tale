1763e7aba9fd8cc26f48970b0a959fa4571daddf
IanKarlo <iankarlots@gmail.com>
2025-07-13 02:10:35 -0300

    chore: Ajuste na forma como usar o typeorm

--- backend/src/config/db/database.config.ts
+++ backend/src/config/db/database.config.ts
@@ -11,8 +11,8 @@ const config = {
     username: `${process.env.DB_USERNAME}`,
     password: `${process.env.DB_PASSWORD}`,
     database: `${process.env.DB_NAME}`,
-    entities: process.env.ENTITIES_PATH ? [process.env.ENTITIES_PATH] : ["dist/core/entities/*.entity.{ts,js}"],
-    migrations: process.env.MIGRATIONS_PATH ? [process.env.MIGRATIONS_PATH] : ["dist/config/db/migrations/*.{ts,js}"],
+    entities: process.env.ENTITIES_PATH && process.env.TYPEORM_CLI != 'true' ? [process.env.ENTITIES_PATH] : ["src/core/entities/*.entity.{ts,js}"],
+    migrations: process.env.MIGRATIONS_PATH && process.env.TYPEORM_CLI != 'true' ? [process.env.MIGRATIONS_PATH] : ["src/config/db/migrations/*.{ts,js}"],
     autoLoadEntities: true,
     synchronize: false,
 }
