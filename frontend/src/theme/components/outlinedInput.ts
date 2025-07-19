c00edf2a05df6319fac05fc96299914621a34c29
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-19 20:15:18 -0300

    feat: Atualizar estilos do componente OutlinedInput para incluir !important nos estados de foco e hover, garantindo a aplicação correta das cores do tema

--- frontend/src/theme/components/outlinedInput.ts
+++ frontend/src/theme/components/outlinedInput.ts
@@ -9,17 +9,26 @@ export const OutlinedInputOverrides: Components['MuiOutlinedInput'] = {
       color: baseTheme.palette.text?.primary,
 
       '& fieldset': {
-        borderColor: baseTheme.palette.primary?.main,
+        borderColor: `${baseTheme.palette.primary?.main} !important`,
       },
       '&:hover fieldset': {
-        borderColor: baseTheme.palette.primary?.light,
+        borderColor: `${baseTheme.palette.primary?.light} !important`,
       },
       '&.Mui-focused fieldset': {
-        borderColor: baseTheme.palette.secondary?.main,
+        borderColor: `${baseTheme.palette.secondary?.main} !important`,
+      },
+      '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: `${baseTheme.palette.primary.light} !important`,
+      },
+      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+        borderColor: `${baseTheme.palette.secondary.main} !important`,
       },
       '& input': {
         color: baseTheme.palette.text?.primary,
       },
+    },
+    notchedOutline: {
+      borderColor: `${baseTheme.palette.primary.main} !important`,
     },
   },
 };
