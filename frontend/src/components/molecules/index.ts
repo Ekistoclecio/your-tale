a4d6577ca58961f2a6e760e1ae557261c12b4d70
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-18 21:01:21 -0300

    feat: Adicionar arquivos de índice para componentes átomos, moléculas e organismos, facilitando a importação e organização dos componentes

---
+++ frontend/src/components/molecules/index.ts
@@ -0,0 +1,6 @@
+export { SessionCard } from './SessionCard';
+export { WelcomeModal } from './WelcomeModal';
+export { EnterCodeModal } from './EnterCodeModal';
+export { EmptyState } from './EmptyState';
+export { AISuggestions } from './AISuggestions';
+export { HeroBanner } from './HeroBanner';
