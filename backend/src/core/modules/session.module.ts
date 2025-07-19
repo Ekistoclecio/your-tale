bb6feabd910d3a96736144c5723508e773bb5341
IanKarlo <iankarlots@gmail.com>
2025-07-19 14:07:43 -0300

    chore: Refatoração da organização dos controllers no código

--- backend/src/core/modules/session.module.ts
+++ backend/src/core/modules/session.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Session } from '../entities/session.entity';
 import { SessionService } from '../providers/session.service';
-import { SessionController } from '../../api/controllers/session.controller';
+import { SessionController } from '../controllers/session.controller';
 import { SessionMemberService } from '../providers/session-member.service';
 import { SessionMember } from '../entities/session-member.entity';
 import { Message } from '../entities/message.entity';
