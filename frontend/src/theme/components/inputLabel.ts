ed029f306aed5ab81ed6ba8f60cb12ded3bbc82b
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-19 20:14:40 -0300

    feat: Atualizar estilos do InputLabel para usar cor secundária e adicionar foco estilizado

--- frontend/src/theme/components/inputLabel.ts
+++ frontend/src/theme/components/inputLabel.ts
@@ -4,13 +4,16 @@ import { baseTheme } from '@/theme/base';
 export const InputLabelOverrides: Components['MuiInputLabel'] = {
   styleOverrides: {
     root: {
-      color: baseTheme.palette.text?.primary,
+      color: baseTheme.palette.text?.secondary,
       fontWeight: baseTheme.typography.fontWeightMedium,
       letterSpacing: '0.03em',
+
+      '&.Mui-focused': {
+        color: baseTheme.palette.secondary?.main,
+      },
     },
     shrink: {
       color: baseTheme.palette.secondary?.main,
-      ...baseTheme.typography.h5,
     },
   },
 };
