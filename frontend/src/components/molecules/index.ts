18c2c7ac17603a567bd7268f715be4a0f72c8f16
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-19 11:28:30 -0300

    feat: Adicionar exportação dos componentes UserProfileMenu e MobileDrawerUserProfileMenu ao índice de moléculas

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,5 +1,6 @@
 export { SessionCard } from './SessionCard';
-export { WelcomeModal } from './WelcomeModal';
 export { EnterCodeModal } from './EnterCodeModal';
 export { MySessionsEmptyState } from './MySessionsEmptyState';
 export { HeroBanner } from './HeroBanner';
+export { UserProfileMenu } from './UserProfileMenu';
+export { MobileDrawerUserProfileMenu } from './MobileDrawerUserProfileMenu';
