84a30a1a6c838b1a393d1f391bfcce835ff83383
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-07-27 13:48:33 -0300

    feat: Implementar autenticação com NextAuth, incluindo middleware de autenticação e suporte a tokens JWT, além de configurar cliente API com Axios

---
+++ frontend/src/services/client.ts
@@ -0,0 +1,70 @@
+import { ApiErrorResponse } from './interfaces';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { getSession, signOut } from 'next-auth/react';
+
+class ApiService {
+  private api: AxiosInstance;
+
+  constructor(entity: string) {
+    this.api = axios.create({
+      baseURL: `${process.env.NEXT_PUBLIC_API_URL}/${entity}`,
+      timeout: 10000,
+    });
+
+    this.api.interceptors.request.use(async (config) => {
+      const session = await getSession();
+      const token = session?.accessToken;
+
+      if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+
+      return config;
+    });
+
+    this.api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && error.response.status === 401) {
+          signOut();
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  public get = async <T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> => {
+    return await this.api.get<T>(url, config);
+  };
+
+  public post = async <T>(
+    url: string,
+    data: object,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> => {
+    return await this.api.post<T>(url, data, config);
+  };
+
+  public patch = async <T>(
+    url: string,
+    data: object,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> => {
+    return await this.api.patch<T>(url, data, config);
+  };
+
+  public delete = async <T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> => {
+    return await this.api.delete<T>(url, config);
+  };
+
+  public static isApiError(error: unknown): error is AxiosError<ApiErrorResponse> {
+    return axios.isAxiosError(error) && !!error.response?.data?.statusCode;
+  }
+}
+
+const ApiServiceInstance = new ApiService('');
+
+export { ApiService, ApiServiceInstance };
