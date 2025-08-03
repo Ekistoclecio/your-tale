764d1aed54821d392299ec9d2d10d357d7c1a2c5
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-08-02 22:42:02 -0300

    feat: Criar nova página de criação de personagem com integração à API e feedback de sucesso/erro

---
+++ frontend/src/app/(private)/session/[id]/create_character/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { CharacterFormTemplate } from '@/components/templates/CharacterFormTemplate';
+import { CreateCharacterFormData } from '@/schemas/form-validation/createCharacterForm';
+import { useSnackbar } from 'notistack';
+import { useCreateCharacter, useRegisterMember } from '@/queries/character/mutation';
+
+export default function CreateCharacterPage() {
+  const { id } = useParams<{ id: string }>();
+  const { enqueueSnackbar } = useSnackbar();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { mutateAsync: createCharacter } = useCreateCharacter();
+  const { mutateAsync: registerMember } = useRegisterMember();
+
+  const handleSubmit = async (data: CreateCharacterFormData) => {
+    setIsLoading(true);
+
+    try {
+      console.log('Dados do personagem:', data);
+      await registerMember(id);
+      const response = await createCharacter({ ...data, session_id: id });
+      console.log('Resposta da API:', response);
+
+      enqueueSnackbar('Personagem criado com sucesso!', { variant: 'success' });
+
+      router.push(`/session/${id}`);
+    } catch (error) {
+      console.error('Erro ao criar personagem:', error);
+      enqueueSnackbar('Erro ao criar personagem. Tente novamente.', { variant: 'error' });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <CharacterFormTemplate onSubmit={handleSubmit} isLoading={isLoading} />
+    </>
+  );
+}
